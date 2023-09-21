a9a836eaec994eff279e59635643b9af16acde7c
wayne-otido <otidowayne@gmail.com>
2023-09-21 11:50:21 +0300

    done

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from datetime import datetime
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
@@ -11,10 +12,27 @@ db = SQLAlchemy(metadata=metadata)
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
+    serialize_rules = ('-baked_goods.bakery',)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default =datetime.now)
+    updated_at = db.Column(db.DateTime, default =datetime.now, onupdate=datetime.now)
+
+    baked_goods = db.relationship('BakedGood', backref='bakery')
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
+    serialize_rules = ('-bakery.baked_goods',)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default =datetime.now)
+    updated_at = db.Column(db.DateTime, default =datetime.now, onupdate=datetime.now)
+
+    bakery_id =  db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+
+    
     
